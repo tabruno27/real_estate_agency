44b1bb06f484139f65c07b4a6ccfba1868d72fa8
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-05-30 17:09:08 +0200

    Add model field Flat.new_building

---
+++ property/migrations/0003_flat_new_building.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('property', '0002_auto_20190829_2242'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='flat',
+            name='new_building',
+            field=models.BooleanField(blank=True, null=True),
+        ),
+    ]
