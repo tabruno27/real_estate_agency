152e390d1b2c5ce3faba4fff31cf5b12ea29faa1
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-05-30 18:35:20 +0200

    setup admin UI for likes.

--- property/admin.py
+++ property/admin.py
@@ -7,9 +7,15 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town','address', 'owner')
     readonly_fields = ["created_at"]
     list_display = ('address', 'price', 'new_building',
-                    'construction_year', 'town')
+                    'construction_year', 'town', 'likes_count')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'town', 'construction_year')
+    raw_id_fields = ('likes',)
+
+
+    def likes_count(self, obj):
+        return obj.likes.count()
+    likes_count.short_description = 'Количество лайков'
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', 'flat')
