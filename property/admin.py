a7b24c6294c89752679cd1acfe26a8efb358fca0
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-06-11 14:17:28 +0200

    Админка чтобы показать собственников

--- property/admin.py
+++ property/admin.py
@@ -3,14 +3,29 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Flat.owners.through
+    extra = 4
+    raw_id_fields = ['owner']
+    verbose_name = 'Собственник'
+    verbose_name_plural = 'Собственники'
+
+    def owner(self, obj):
+        return obj.owner.full_name
+
+    owner.short_description = 'Имя владельца'
+
+
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town','address', 'owner')
+    search_fields = ('town', 'town_district', 'address',)
     readonly_fields = ["created_at"]
     list_display = ('address', 'price', 'new_building',
                     'construction_year', 'town', 'likes_count')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'town', 'construction_year')
-    raw_id_fields = ('likes',)
+    raw_id_fields = ('liked_by',)
+    inlines = [OwnerInline]
+    exclude = ['owners']
 
 
     def likes_count(self, obj):
