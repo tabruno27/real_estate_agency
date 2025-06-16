239e2e50d8bf8188b2809dfc00a9d31914105b8a
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-06-16 11:05:42 +0200

    Update 0007_auto_20250531_1441.py

--- property/migrations/0007_auto_20250531_1441.py
+++ property/migrations/0007_auto_20250531_1441.py
@@ -5,7 +5,7 @@ import phonenumbers
 
 def populate_owner_pure_phone(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
+    for flat in Flat.objects.all().iterator():
         if flat.owners_phonenumber:
             try:
                 parsed_number = phonenumbers.parse(flat.owners_phonenumber, 'RU')
